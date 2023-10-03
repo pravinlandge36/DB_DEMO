cb223e03dcdd3c8af67914a1123de1ae10a5d9a3
Pravin Landge <ws_htu1000@hoonartek03.net>
2023-10-03 11:03:29 +0530

    third commit

--- demo_1/models/example/my_second_dbt_model.sql
+++ demo_1/models/example/my_second_dbt_model.sql
@@ -1,7 +1,6 @@
 {{ config(materialized='table') }}
--- Use the `ref` function to select from other models
 
 
 SELECT students.student_id, student_name, student_age, course_name
-FROM DEMO.PUBLIC.students
-INNER JOIN DEMO.PUBLIC.courses ON students.student_id = courses.student_id
+FROM test.PUBLIC.students
+INNER JOIN test.PUBLIC.courses ON students.student_id = courses.student_id
