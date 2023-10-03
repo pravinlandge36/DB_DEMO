66d91ae4c8dbdec70a21c7e8fc5bd1d1fe9d3ab7
Pravin Landge <ws_htu1000@hoonartek03.net>
2023-10-03 10:20:46 +0530

    second commit

--- demo_1/models/example/my_second_dbt_model.sql
+++ demo_1/models/example/my_second_dbt_model.sql
@@ -2,5 +2,4 @@
 -- Use the `ref` function to select from other models
 
 select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+from TEST.PUBLIC.EMPLOYEE e inner join TEST.PUBLIC.EMPLOYEE_DATA d on e.empid=d.empid order by desc
